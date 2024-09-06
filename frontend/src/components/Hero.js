64386df6d5f4359dbe133f8dfd4e0832e2f43663
manankochar <143321344+manankochar@users.noreply.github.com>
2024-09-06 19:57:17 +0530

    bg added

---
+++ frontend/src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='container'>
+        <div className='text-center text-muted mt-5'>
+            <h3>YOUR MENTAL</h3>
+            <h1>HEALTH MATTERS</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Hero from './Hero'
 function App() {
   return (
     <>
         <Navbar/>
+        <Hero/>
     </>
   )
 }
+
 
 export default App
