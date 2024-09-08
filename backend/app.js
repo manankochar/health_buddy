7c6b064a5509aa969b745299dd1c7bb73469a22f
manankochar <143321344+manankochar@users.noreply.github.com>
2024-09-08 16:54:33 +0530

    backend & imgs

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,21 @@
-const express = require("express");
-const mongoose = require("mongoose");
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 8080;
+const uri = process.env.MONGO_URL;
+
 const app = express();
 
-async function main() {
-    try {
-        await mongoose.connect("mongodb+srv://admin:admin@123@healthbuddy.zzpqo.mongodb.net/?retryWrites=true&w=majority&appName=healthbuddy", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        
-        // Server
-        const port = 8080;
-        app.listen(port, () => {
-            console.log(`App is running on port ${port}`);
-        });
-    } catch (err) {
-        console.error("Failed to connect to the database", err);
-    }
-}
-
-main();
+mongoose.connect(uri)
+   .then(() => {
+       console.log('Connected to MongoDB');
+       app.listen(PORT, () => {
+           console.log(`Server started on port ${PORT}`);
+       });
+   })
+   .catch(err => {
+       console.error('MongoDB connection error:', err);
+       process.exit(1); 
+   });
